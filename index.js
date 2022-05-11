2194f6e20353f7071a03aaca1a471c795bb4b45b
Naftali Kulik <nkulik1994@gmail.com>
2022-05-11 13:59:44 -0400

    commit tree cf267d2c42702bb2e878fd1f3fb9d20626f0ce46

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, "")
+  }
+
+  static titleize(string) {
+    const stringArr = string.split(' ')
+    const newString = []
+    stringArr.map(word => {
+      switch (word) {
+        case 'a':
+        case 'an':
+        case 'but':
+        case 'of':
+        case 'and':
+        case 'for':
+        case 'at':
+        case 'by':
+        case 'from':
+        case 'the':
+          newString.push(word)
+          break;
+        default:
+          newString.push(Formatter.capitalize(word))
+      }
+    })
+    newString.splice(0, 1, Formatter.capitalize(newString[0]))
+    return newString.join(' ')
+  }
 }
